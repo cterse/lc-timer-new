3e32ddb86d12e43eef2d1d4ef0b74bec2a20c8fb
neil4droid <cterse@gmail.com>
2020-05-20 01:07:44 +0530

    Change problem collection objbect type to Map

--- content-scripts/ProblemCollection.ts
+++ content-scripts/ProblemCollection.ts
@@ -4,89 +4,90 @@ import { Session } from "./Session";
 import { ChromeStorageResult, ChromeStorageProblem } from "./ChromeStorageTypes";
 import { ProblemUtils } from "./ProblemUtils";
 
-interface ProblemCollectionObject {
-    [key: string]: Problem;
-}
-
 export class ProblemCollection {
-    private problemCollectionObject: ProblemCollectionObject;
+    private problemCollectionObject: Map<number, Problem>;
     private activeProblemCount: number = 0;
     private completeProblemCount: number = 0;
     private totalProblemCount: number = 0;
 
     constructor(storageResult?: ChromeStorageResult) {
-        this.problemCollectionObject = storageResult ? this.contructCollectionObjectFromChromeResult(storageResult) : {};
+        this.problemCollectionObject = storageResult ? this.contructProblemsMapFromChromeResult(storageResult) : new Map<number, Problem>();
+        this.updateProblemCount();
     }
 
-    private contructCollectionObjectFromChromeResult(storageResult: ChromeStorageResult): ProblemCollectionObject {
+    private contructProblemsMapFromChromeResult(storageResult: ChromeStorageResult): Map<number, Problem> {
         let chromeCollectionObject = storageResult[Constants.STORAGE_PROBLEM_COLLECTION];
-        let pcObject: ProblemCollectionObject = {};
+        let pcObject = new Map<number, Problem>();
         let problemUtils = new ProblemUtils();
 
         for (let key in chromeCollectionObject) {
             if (!chromeCollectionObject.hasOwnProperty(key)) continue;
 
             let storageProblem = chromeCollectionObject[key];
-            let code: number = problemUtils.getProblemCodeFromStorageProblem(storageProblem);
-            let name: string = problemUtils.getProblemNameFromStorageProblem(storageProblem);
-            let url: URL = problemUtils.getProblemUrlFromStorageProblem(storageProblem);
-            let sessionsList: Array<Session> = problemUtils.getProblemSessionListFromStorageProblem(storageProblem);
+            let code = problemUtils.getProblemCodeFromStorageProblem(storageProblem);
+            let name = problemUtils.getProblemNameFromStorageProblem(storageProblem);
+            let url = problemUtils.getProblemUrlFromStorageProblem(storageProblem);
+            let sessionsList = problemUtils.getProblemSessionListFromStorageProblem(storageProblem);
             let problem = new Problem(code, name, url, sessionsList);
 
-            pcObject[problem.getCode()] = problem;
+            pcObject.set(problem.getCode(), problem);
         }
 
         return pcObject;
     }
 
-    getProblemCollectionObject(): ProblemCollectionObject {
+    getProblemCollectionObject(): Map<number, Problem> {
         return this.problemCollectionObject;
     }
 
     setProblemCollectionObject(storageResult: ChromeStorageResult): ProblemCollection {
+        this.problemCollectionObject = this.contructProblemsMapFromChromeResult(storageResult);
+        return this;
     }
 
     getProblem(problemCode: number): Problem | undefined {
-        return this.problemCollectionObject[problemCode];
+        return this.problemCollectionObject.get(problemCode);
     }
 
     addProblem(problem: Problem): Problem {
-        this.problemCollectionObject[problem.getCode()] = problem;
+        this.problemCollectionObject.set(problem.getCode(), problem);
         this.updateProblemCount();
         return problem;
     }
 
-    removeProblem(problemCode: number): Problem | null {
-        let deletedProblem = this.problemCollectionObject[problemCode];
-        delete this.problemCollectionObject[problemCode];
+    removeProblem(problemCode: number): Problem | undefined {
+        let deletedProblem = this.problemCollectionObject.get(problemCode);
+        this.problemCollectionObject.delete(problemCode);
         this.updateProblemCount();
         return deletedProblem;
     }
 
     updateProblemCount(): void {
         let activeCount = 0, completeCount = 0, total = 0;
-        for (let key in this.problemCollectionObject) {
-            if (!this.problemCollectionObject.hasOwnProperty(key)) continue;
-            let problemObj = this.problemCollectionObject[key];
-            let problem: Problem = problemObj;
-            if (problem.getStatus() === Constants.PROBLEM_STATUS_ACTIVE) activeCount++;
-            if (problem.getStatus() === Constants.PROBLEM_STATUS_COMPLETE) completeCount++;
+
+        for (let problem of this.problemCollectionObject.values()) {
+            if (problem.isActive()) activeCount++;
+            if (problem.isComplete()) completeCount++;
             total++;
         }
+        
         this.activeProblemCount = activeCount;
         this.completeProblemCount = completeCount;
         this.totalProblemCount = total;
     }
 
     getActiveProblemCount(): number {
+        this.updateProblemCount();
         return this.activeProblemCount;
     }
 
     getCompleteProblemCount(): number {
+        this.updateProblemCount();
         return this.completeProblemCount;
     }
 
     getTotalProblemCount(): number {
+        this.updateProblemCount();
         return this.totalProblemCount;
     }
 }
--- spec/ProblemUtilsSpec.ts
+++ spec/ProblemUtilsSpec.ts
@@ -1,13 +1,33 @@
 import { Constants } from "../content-scripts/Constants";
 import { ProblemUtils } from "../content-scripts/ProblemUtils";
-import { ChromeStorageProblem } from "../content-scripts/ChromeStorageTypes";
+import { ChromeStorageProblem, ChromeStorageSession } from "../content-scripts/ChromeStorageTypes";
+import { Session } from "../content-scripts/Session";
 
 describe('ProblemUtils Test Suits', () => {
     var problemUtils = new ProblemUtils();
 
+    describe('ProblemUtils.getSessionFromStorageSession() method', () => {
+        it('should return a Session object given a ChromeStorageSession object', () => {
+            let ts = Date.now();
+            let storageSession: ChromeStorageSession = {
+                [Constants.STORAGE_SESSION_ID]: '1-1',
+                [Constants.STORAGE_SESSION_INIT_TS]: ts,
+                [Constants.STORAGE_SESSION_END_TS]: ts+1000,
+                [Constants.STORAGE_SESSION_STATUS]: Constants.SESSION_STATUS_COMPLETE
+            }
+
+            let session = problemUtils.getSessionFromStorageSession(storageSession);
+
+            expect(session.getId()).toBe('1-1');
+            expect(session.getInitTimestamp()).toBe(ts);
+            expect(session.getEndTimestamp()).toBe(ts+1000);
+            expect(session.getStatus()).toBe(Constants.SESSION_STATUS_COMPLETE);
+        });
+    });
+
     describe('ProblemUtils.getProblemSessionListFromStorageProblem() method', () => {
         it('should return an array of Sessions from the storage problem\'s session array', () => {
-            let storageSessionList = [
+            let storageSessionList: Array<ChromeStorageSession> = [
                 {
                     [Constants.STORAGE_SESSION_ID]: '1-1',
                     [Constants.STORAGE_SESSION_INIT_TS]: Date.now(),
@@ -38,7 +58,8 @@ describe('ProblemUtils Test Suits', () => {
 
             for (var i=0; i<sessionList.length; i++) {
                 let s = sessionList[i];
-                console.log(typeof s);
+                expect(s).toEqual(jasmine.any(Session));
+                expect(s.getId()).toBe('1-'+(i+1));
             }
         });
     });
