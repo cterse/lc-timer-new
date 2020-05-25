b3f6d1206be974fd58281ac24e269e09f8a76210
neil4droid <cterse@gmail.com>
2020-05-25 23:27:25 +0530

    Complete new problem content script

--- content-scripts/NewProblemContentScript.ts
+++ content-scripts/NewProblemContentScript.ts
@@ -1,6 +1,7 @@
 import { Constants } from "./Constants";
 import { ProblemCollection } from "./ProblemCollection";
 import { ProblemUtils } from "./ProblemUtils";
+import { ProblemConverter } from "./ProblemConverter";
 import { ChromeStorageResult } from "./ChromeStorageTypes";
 
 let pageInitInterval = setInterval(setupMonitoringScript, 100);
@@ -9,20 +10,26 @@ function setupMonitoringScript() {
     if ($(Constants.PROBLEM_TITLE_SELECTOR).length) {
         clearInterval(pageInitInterval);
 
-        chrome.storage.sync.get([Constants.STORAGE_PROBLEM_COLLECTION], function(result){
+        chrome.storage.sync.get([Constants.STORAGE_PROBLEM_COLLECTION], function(result: ChromeStorageResult){
             let currentTime = Date.now();
             
-            if (!result || !result[Constants.STORAGE_PROBLEM_COLLECTION]) {
+            if (!result) {
                 console.error("lc-timer-new:problem-content : Error getting result from sync.");
                 return null;
             }
-            
-            let problemUtils = new ProblemUtils();
-            let problemsCollection = new ProblemCollection(result);
-            let storageUpdated = false;
 
             try {
+                let problemUtils = new ProblemUtils();
+                let converter = new ProblemConverter();
+                let problemsCollection = null;
                 let contextProblem = problemUtils.createProblemFromContext().start();
+                let storageUpdated = false;
+
+                if (result[Constants.STORAGE_PROBLEM_COLLECTION]) {
+                    problemsCollection = new ProblemCollection(result, converter);
+                } else {
+                    problemsCollection = new ProblemCollection();
+                }
 
                 if (problemsCollection.getProblem(contextProblem.getCode())) {
                     let problem = problemsCollection.getProblem(contextProblem.getCode());
@@ -38,15 +45,16 @@ function setupMonitoringScript() {
                     problemsCollection.addProblem(contextProblem);
                     storageUpdated = true;
                 }
+
+                if(storageUpdated) {
+                    let csStorageCollection = converter.problemCollectionMapToChromeCollection(problemsCollection.getProblemCollectionMap());
+                    chrome.storage.sync.set({[Constants.STORAGE_PROBLEM_COLLECTION]:  csStorageCollection}, function(){
+                        console.debug("lc-timer:problem-content : set data to storage.");
+                        console.dir(csStorageCollection);
+                    });
+                }
             } catch (error) {
                 console.error(error.message);
-            }
-            
-            if(storageUpdated) {
-                chrome.storage.sync.set({[Constants.STORAGE_PROBLEM_COLLECTION]:  problemsCollection}, function(){
-                    console.debug("lc-timer:problem-content : set data to storage.");
-                    console.dir(problemsCollection);
-                });
             }
         });
     }
